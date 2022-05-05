92ebe7078485f2b19232079a7f5449ce5eac0377
jrvalencia <jrodrivaldia@hotmail.com>
2022-05-04 22:54:28 -0500

    Enddate class

---
+++ VS2022/VS2022/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VS2022;
+
+Console.WriteLine("Hello, jrvalencia!");
+Console.WriteLine("==================");
+/*Date date1 = new Date(2022, 5, 4);
+Console.WriteLine(date1);*/
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
